8b1e205145c3e2320458bfc617b24e3849411cdf
Jerry Tabernero <tabernerojerry@gmail.com>
2020-10-04 18:03:05 +0800

    add api global prefix

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,18 @@
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const globalPrefix = 'api';
+  const port = process.env.PORT || 3000;
+
+  app.setGlobalPrefix(globalPrefix);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+
+  await app.listen(port, () => {
+    Logger.log(`Listening at http:localhost:${port}/${globalPrefix}`);
+  });
 }
 bootstrap();
