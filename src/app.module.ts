47d7f3a3c4a08162223130950821956072b54fbe
Jerry Tabernero <tabernerojerry@gmail.com>
2020-10-05 21:24:34 +0800

    added typeorm and connect to db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
 
 @Module({
-  imports: [CoffeesModule],
+  imports: [
+    CoffeesModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'qwerty123',
+      database: 'postgres',
+      autoLoadEntities: true,
+      // Make sure disable synchronize in production
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
