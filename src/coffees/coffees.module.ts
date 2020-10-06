1ef671262e87043bd7a53193a7932d0e9935784c
Jerry Tabernero <tabernerojerry@gmail.com>
2020-10-06 20:33:58 +0800

    create typeorm entity

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { CoffeesController } from './coffees.controller';
 import { CoffeesService } from './coffees.service';
+import { CoffeeEntity } from './entities/coffee.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([CoffeeEntity])],
   providers: [CoffeesService],
   controllers: [CoffeesController],
 })
--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -1,6 +1,16 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('coffees')
 export class CoffeeEntity {
+  @PrimaryGeneratedColumn()
   readonly id: number;
+
+  @Column()
   readonly name: string;
+
+  @Column()
   readonly brand: string;
+
+  @Column('json', { nullable: true })
   readonly flavors: string[];
 }
