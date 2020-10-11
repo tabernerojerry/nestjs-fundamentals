167910b75bf2ed454737a4bcb297f0c4163efc21
Jerry Tabernero <tabernerojerry@gmail.com>
2020-10-11 17:36:24 +0800

    added config module and use env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,16 +3,18 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { CoffeesModule } from './coffees/coffees.module';
 import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'qwerty123',
-      database: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: +process.env.DATABASE_PORT,
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
       autoLoadEntities: true,
       // Make sure to disable synchronize in production
       synchronize: true,
