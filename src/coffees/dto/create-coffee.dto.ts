69b0a5577e43189582525c2322b57f7bef89401d
Jerry Tabernero <tabernerojerry@gmail.com>
2020-10-18 17:05:18 +0800

    add swagger ApiProperty to models

--- src/coffees/dto/create-coffee.dto.ts
+++ src/coffees/dto/create-coffee.dto.ts
@@ -1,12 +1,16 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsOptional, IsString } from 'class-validator';
 
 export class CreateCoffeeDto {
+  @ApiProperty({ description: 'Name of coffee.' })
   @IsString()
   readonly name: string;
 
+  @ApiProperty({ description: 'Brand name of coffee' })
   @IsString()
   readonly brand: string;
 
+  @ApiProperty({ example: [] })
   @IsString({ each: true })
   readonly flavors: string[];
 
