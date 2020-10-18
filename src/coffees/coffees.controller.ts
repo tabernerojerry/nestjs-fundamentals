d6066c2760867371460340e0f765c2e3b8e091b2
Jerry Tabernero <tabernerojerry@gmail.com>
2020-10-18 17:10:41 +0800

    add swagger ApiForbiddenResponse

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -15,11 +15,13 @@ import { CoffeesService } from './coffees.service';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 import { Protocol } from '../common/decorators/protocol.decorator';
+import { ApiForbiddenResponse } from '@nestjs/swagger';
 
 @Controller('coffees')
 export class CoffeesController {
   constructor(private readonly coffeeService: CoffeesService) {}
 
+  @ApiForbiddenResponse({ description: 'Forbidden' })
   @Public()
   @Get()
   public findAll(
